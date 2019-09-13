93e4ab703d343ee178b5b1666c8d617296568bc0
Andrii <A.I.Khmelovsky@gmail.com>
2019-09-13 12:18:54 +0300

    retrive data products

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+
+
+
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const objData = JSON.parse(data);
 
@@ -9,13 +30,21 @@ const objData = JSON.parse(data);
 const server = http.createServer((req,res) => {
     const path = req.url;
 
-    if(path === '/' || path === '/home')
+    if(path === '/' || path === '/overview')
     {
-        res.end('<h1>Home Page</h1>')
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        const cards = objData.map((el) => {
+           return replaceTemplate(tempCard, el);
+        });
+        
+        const outPut = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cards);
+
+        res.end(outPut);
     }
-    else if(path === '/users')
+    else if(path === '/product')
     {
-        res.end('<h1>users Page</h1>')
+        res.end(tempProduct);
     }
     else if(path === '/api') {
         res.writeHead(200, {
